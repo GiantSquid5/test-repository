3fb39e4a75c4fe4bff3267b85cdadf097a7ad7c8
GiantSquid5 <78445678+GiantSquid5@users.noreply.github.com>
2021-05-31 22:20:19 -0400

    HelloWorldCommit

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.listen(3000, () => console.log('App listening for connections'))
+
 console.log("Does this work?");
