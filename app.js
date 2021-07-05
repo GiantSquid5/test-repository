e618203f31c109e08ff2c023580852770d5a1ef9
GiantSquid5 <gideongish@gmail.com>
2021-07-05 15:35:22 -0400

    Pushing from sourcetree

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World From GitHub! Updated but this time we push!'))
+app.get('/', (req, res) => res.send('Hello World From Sourcetree!!'))
 // go into folder on the server(test-repository) and do git pull
 //edits inside of the git repository on the server whould be collected using git pull
 
 //sourcetree can collect updates from git using the pull button when accessing the repository from local (as long as it connected and hasnt been moved)
+//and it updates even when the file is open
 
+//you can also send saveed files from sourcetree by saving it in your preferred editor from the git repo
+//and of course saving and pushing
 app.listen(3000, () => console.log('App listening for connections'))
 
 console.log("Does this work?");
