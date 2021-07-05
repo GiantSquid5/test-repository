f24a5e677fb1e3431d5073e5c4205baf911e5e3a
GiantSquid5 <78445678+GiantSquid5@users.noreply.github.com>
2021-07-05 15:27:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World From GitHub! Updated but this time we push!'))
 // go into folder on the server(test-repository) and do git pull
+//edits inside of the git repository on the server whould be collected using git pull
 
 app.listen(3000, () => console.log('App listening for connections'))
 
