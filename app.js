d00658a18337cbf978ac13ea06a3766d2b2e1a9a
GiantSquid5 <78445678+GiantSquid5@users.noreply.github.com>
2021-07-05 15:19:52 -0400

    Git Pull updates, but does push

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World From GitHub! But Updated!'))
+app.get('/', (req, res) => res.send('Hello World From GitHub! Updated but This time through push!'))
+// go into folder(test-repository) and do git pull
 
 app.listen(3000, () => console.log('App listening for connections'))
 
