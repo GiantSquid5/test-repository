299fa7a2ffb1cb7b08825995fa7c2eed730f21c0
GiantSquid5 <78445678+GiantSquid5@users.noreply.github.com>
2021-07-05 15:30:37 -0400

    SourceTree Test

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get('/', (req, res) => res.send('Hello World From GitHub! Updated but this t
 // go into folder on the server(test-repository) and do git pull
 //edits inside of the git repository on the server whould be collected using git pull
 
+//sourcetree can collect updates from git using the pull button when accessing the repository from local (as long as it connected and hasnt been moved)
+
 app.listen(3000, () => console.log('App listening for connections'))
 
 console.log("Does this work?");
