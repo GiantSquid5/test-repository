d9d18146335b1cfd38c8c6585e380d89bb76b007
GiantSquid5 <gideongish@gmail.com>
2021-07-13 11:40:08 -0400

    no message

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('test'))
 //app.get('/style', (req, res) => res.render('style.css'))
-app.get('/style', (req, res) => res.sendFile('./views/style.css'))
+app.get('/style', (req, res) => res.sendFile(path.join(__dirname+'/views/style.css')))
 
 // go into folder on the server(test-repository) and do git pull
 //edits inside of the git repository on the server whould be collected using git pull
